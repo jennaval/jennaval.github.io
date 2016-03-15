18de799bba6a9822721a94d09535a7a74e05e562
jennaval <jennavalaydon@Jennas-MacBook-Pro-2.local>
2016-03-15 11:30:44 -0400

    sync

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,6 +1,15 @@
 $("#getClients").on("click", function() {
+  // alert("working");
+
+  var url = "http://jennaval.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
+  $.getJSON(url, function(data) {
+
+    alert(data);
+    console.dir(data);
 
 
-  alert("working");
+  })
+
+
 
 })
