e2aa20c4d15e934fc7f91f7d2818c7af9962baad
jennaval <jennavalaydon@Jennas-MacBook-Pro-2.local>
2016-03-15 11:39:53 -0400

    sync

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,15 +1,22 @@
+$(document).ready(function(){
+
 $("#getClients").on("click", function() {
   // alert("working");
 
   var url = "http://jennaval.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json"
   $.getJSON(url, function(data) {
 
-    alert(data);
-    console.dir(data);
+    // alert(data);
+    // console.dir(data);
+    $.each(data, function(index, item){
+
+      $("#data").append(item.name);
+    })
+
+  }) // getJSON
 
 
-  })
 
+}) //click
 
-
-})
+});
