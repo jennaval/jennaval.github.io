f73175e88c1b73301a5788c3e335cf3bae04e07d
jennaval <jennavalaydon@Jennas-MacBook-Pro-2.local>
2016-03-22 09:10:15 -0400

    json

--- otherProjects/ajaxExamples/js/jsonCATS.js
+++ otherProjects/ajaxExamples/js/jsonCATS.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   // alert("J");
   // $.getJSON
-  $.getJSON("jsonDatabase/jsonCATS.json", function(data){
+  $.getJSON("../jsonDatabase/jsonCATS.json", function(data){
 
     console.dir(data);
 
